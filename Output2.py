46b7957413b81221d237855c920776825bbdfce1
Julius-Okeyo <jaykopiyo@gmail.com>
2022-09-11 13:23:53 +0300

    Updated Output 2

--- Output2.py
+++ Output2.py
@@ -1,6 +1,16 @@
-'''Due to the counter-intuitive behavior of arbitrary-precision
-arithmetic(long arithmetic), python supports very large integers
-wit long type but the limit of float precision is (2^53)+1.'''
+'''
+Python supports very large integers
+with long type but the limit of float
+precision is (2^53)+1.
+'''
+# << operator shifts bitwise representation
+#of input left by pushing zeros in from the
+#right and let the leftmost bits fall off.
+
+# >> operator shift right by pushing copies
+# of the leftmost bit in from the left, and
+# let the rightmost bits fall off.
+print(bin(1))
 a = (1<<53)+1
-print(a)
-#print(1+1.0>a)
+print(bin(a))
+print(1+1.0>a)
