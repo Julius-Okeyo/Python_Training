39eb19d6f9060577600fc1f451bd71c2a14bf1ec
Julius-Okeyo <jaykopiyo@gmail.com>
2022-09-17 16:34:35 +0300

    Updated Upper vs Capitalized

--- Upper_vs_Capitalize.py
+++ Upper_vs_Capitalize.py
@@ -1,3 +1,8 @@
 string = 'this is PYTHON'
 print(string.capitalize())
+#capitalize() changes the first letter of the string
+# to uppercase and the rest to lowercase.
+
 print(string.upper())
+#upper() changes all the characters of the string
+# to uppercase.
